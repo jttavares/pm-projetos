412ba85fde91c9529d4f3d07ba6c6821e8a991e0
jttavares <32249679+jttavares@users.noreply.github.com>
2018-06-18 22:38:50 -0300

    "completando calcPoupa"

--- calcPoupa/script.js
+++ calcPoupa/script.js
@@ -2,15 +2,21 @@ const divida=$('#txtDivida');
 const prazo=$('#txtPrazo');
 const parcela=$('#txtPrestacao');
 const negativo=$('#txtNegativo');
-const btn=$('button');
+const btnCalculo=$('#calculo');
+const btnLimpar=$("#clear");
 const texto=$('#resultado');
+//const inputs=document.querySelectorAll('input');
 
 //Eventos
+btnCalculo.addEventListener("click",onClickCalculo);
 
+btnLimpar.addEventListener("click",onClickClear);
 
 function $(element){
     return document.querySelector(element);
 }
+
+
 
 function onClickCalculo(){
     let valorDivida=divida.value;
@@ -21,7 +27,14 @@ function onClickCalculo(){
     
         //montante= prestacao*taxa*prazo
        //taxa=((parcelaDesejada-negativo)*prazo)/divida
-        taxa=((((valorParcela*valorPrazo)/valorDivida)-1)*100);
-        texto.innerHTML=`A taxa deve ser de ${taxa.toLocaleString("pt-br")}%`;
+        taxa=((((valorParcela*valorPrazo)/valorDivida)-1)*100)/valorPrazo;
+        texto.innerHTML=`A taxa mensal deve ser de ${taxa.toLocaleString("pt-br")}% a.m`;
 
+}
+function onClickClear(){
+    divida.value="";
+    prazo.value="";
+    parcela.value="";
+    negativo.value="";
+    texto.innerHTML="";
 }

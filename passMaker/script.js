bb6b3deae8bbe12cdffab97860d15fe9707db722
jttavares <32249679+jttavares@users.noreply.github.com>
2022-07-25 18:57:35 -0300

    Adicionado opções de construção de senha

--- passMaker/script.js
+++ passMaker/script.js
@@ -1,13 +1,19 @@
 let caracteres, pass, rand, text;
 const  result=document.querySelector('p');
 const button=document.querySelector('button');
+const inputNumbers = document.getElementById('numbers');
+const inputStrings = document.getElementById('strings');
+const inputSymbols = document.getElementById('symbols');
 
-caracteres=[
-    "!", "@", "#", "$", "%", "*","_", "-", "+", "/", "\\","|",
-                "0", "1", "2", "3", "4", "5","6", "7", "8", "9", 
-                "a", "b", "c", "d", "e", "f","g", "h", "i", "j",  "k", "l", "m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-                "A", "B", "C", "D", "E", "F","G", "H", "I", "J","K", "L", "M", "N","O", "P", "Q", "R", "S", "T","U", "V", "W", "X","Y", "Z"
-];
+const inputQtde = document.getElementById('quantidade');
+
+const numbersCollection = ["0", "1", "2", "3", "4", "5","6", "7", "8", "9"];
+
+const stringsCollection = ["a", "b", "c", "d", "e", "f","g", "h", "i", "j",  "k", "l", "m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+"A", "B", "C", "D", "E", "F","G", "H", "I", "J","K", "L", "M", "N","O", "P", "Q", "R", "S", "T","U", "V", "W", "X","Y", "Z"];
+const symbolsCollections =["!", "@", "#", "$", "%", "*","_", "-", "+", "/", "\\","|"];
+
+caracteres=[];
 
 
 
@@ -21,8 +27,21 @@ text="";
 function generatePass(){
     result.innerHTML="";
     text="";
-    for(let i=0; i<12;i++){
-    pass[i]=caracteres[Math.floor((Math.random()*74))];
+    if(inputNumbers.checked){
+        caracteres = [...numbersCollection];
+    }
+
+    if(inputStrings.checked){
+        caracteres = [...caracteres,...stringsCollection];
+    }
+
+    if(inputSymbols.checked){
+        caracteres = [...caracteres,...symbolsCollections];
+    }
+    
+    let qtde = parseInt(inputQtde.value);
+    for(let i=0; i < qtde;i++){
+    pass[i]=caracteres[Math.floor((Math.random()*caracteres.length))];
     text+=pass[i];
 }
 result.innerHTML=text;
